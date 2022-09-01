a5ffccc6daa109bf27f63949f35cdc4a9ac35db6
Vitaliy Vaskin <vital0101@yandex.ru>
2022-09-01 15:53:08 +0300

    add mask for tel

--- source/js/modules/mask-for-tel/mask-for-tel.js
+++ source/js/modules/mask-for-tel/mask-for-tel.js
@@ -1,85 +1,40 @@
 function showMaskTel() {
-  document.addEventListener('DOMContentLoaded', function () {
-    const phoneInputs = document.querySelectorAll('input[data-tel-input]');
-    // console.log(phoneInputs);
-
-    const getInputNumbersValue = function (input) {
-      // Return stripped input value — just numbers
-      return input.value.replace(/\D/g, '');
-    };
-
-    const onPhonePaste = function (e) {
-      let input = e.target;
-      let inputNumbersValue = getInputNumbersValue(input);
-      let pasted = e.clipboardData || window.clipboardData;
-      if (pasted) {
-        let pastedText = pasted.getData('Text');
-        if (/\D/g.test(pastedText)) {
-          // Attempt to paste non-numeric symbol — remove all non-numeric symbols,
-          // formatting will be in onPhoneInput handler
-          input.value = inputNumbersValue;
-          return;
-        }
-      }
-    };
-
-    const onPhoneInput = function (e) {
-      let input = e.target;
-      const inputNumbersValue = getInputNumbersValue(input);
-      const selectionStart = input.selectionStart;
-      let formattedInputValue = '';
-
-      if (!inputNumbersValue) {
-        input.value = ''; // return
-      }
-
-      if (input.value.length !== selectionStart) {
-        // Editing in the middle of input, not last symbol
-        if (e.data && /\D/g.test(e.data)) {
-          // Attempt to input non-numeric symbol
-          input.value = inputNumbersValue;
-        }
+  const eventCalllback = function (e) {
+    const el = e.target;
+    const clearVal = el.dataset.phoneClear;
+    const pattern = el.dataset.phonePattern;
+    const matrixDef = '+7(___) ___-__-__';
+    const matrix = pattern ? pattern : matrixDef;
+    let i = 0;
+    let def = matrix.replace(/\D/g, '');
+    let val = e.target.value.replace(/\D/g, '');
+    if (clearVal !== 'false' && e.type === 'blur') {
+      if (val.length < matrix.match(/([\_\d])/g).length) {
+        e.target.value = '';
         return;
       }
-
-      if (['7', '8', '9'].indexOf(inputNumbersValue[0]) > -1) {
-        if (inputNumbersValue[0] === '9') {
-          inputNumbersValue = '7' + inputNumbersValue;
-        }
-        let firstSymbols = (inputNumbersValue[0] === '8') ? '8' : '+7';
-        formattedInputValue = input.value = firstSymbols + '';
-        if (inputNumbersValue.length > 1) {
-          formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
-        }
-        if (inputNumbersValue.length >= 5) {
-          formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
-        }
-        if (inputNumbersValue.length >= 8) {
-          formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
-        }
-        if (inputNumbersValue.length >= 10) {
-          formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
-        }
-      } else {
-        formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
-      }
-      input.value = formattedInputValue;
-    };
-
-    const onPhoneKeyDown = function (e) {
-      // Clear input after remove last symbol
-      let inputValue = e.target.value.replace(/\D/g, '');
-      if (e.keyCode === 8 && inputValue.length === 1) {
-        e.target.value = '';
-      }
-    };
-
-    for (const phoneInput of phoneInputs) {
-      phoneInput.addEventListener('keydown', onPhoneKeyDown);
-      phoneInput.addEventListener('input', onPhoneInput, false);
-      phoneInput.addEventListener('paste', onPhonePaste, false);
     }
-  });
+    if (def.length >= val.length) {
+      val = def;
+    }
+    e.target.value = matrix.replace(/./g, function (a) {
+      if (/[_\d]/.test(a) && i < val.length) {
+        return val.charAt(i++);
+      } else {
+        if (i >= val.length) {
+          return '';
+        } else {
+          return a;
+        }
+      }
+    });
+  };
+  const phoneInputs = document.querySelectorAll('[data-phone-pattern]');
+  for (let elem of phoneInputs) {
+    for (let ev of ['input', 'blur', 'focus']) {
+      elem.addEventListener(ev, eventCalllback);
+    }
+  }
 }
 
 export {showMaskTel};
